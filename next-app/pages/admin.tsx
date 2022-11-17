ea599b677965e249b23ddc2a745b41a61ebb2ac3
Mattegbg <mathias.delic977@gmail.com>
2022-11-17 10:21:18 +0100

    ..

--- next-app/pages/admin.tsx
+++ next-app/pages/admin.tsx
@@ -1,10 +1,37 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Router from 'next/router';
+import { useEffect } from 'react';
 import styles from '../styles/Home.module.css'
 
 const Admin: NextPage = () => {
+
+      //blockerar inlogg via URL / kollar så att du är inloggad via token.
+
+      useEffect(() => {
+        async function isLoggedIn() {
+            const response = await fetch('http://localhost:4000/api/loggedin', {
+                credentials: 'include'
+            });
+            const data = await response.json();
+
+            console.log(data);
+
+            if (data.loggedIn == false) {
+                Router.push('/')
+            }
+        }
+
+        isLoggedIn();
+
+    }, [])
+
+
+
   return <div>Admin Page</div>
 };
     
 export default Admin;
+
+
